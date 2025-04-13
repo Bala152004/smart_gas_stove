88c91ace22158179b38b7abb08a098ce1281673c
Bala152004 <balamuruganmanivel2004@gmail.com>
2025-04-13 14:20:12 +0530

    first version my lil web

---
+++ nav.js
@@ -0,0 +1,9 @@
+alert
+function toggleMenu() {
+  const menu = document.getElementById('navMenu');
+  menu.classList.toggle('active');
+}
+window.onload = function () {
+    alert("Welcome to Smart Gas Stove!");
+  };
+  
